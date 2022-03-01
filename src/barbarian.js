27724cad52e57240eda5238d7c63ede575a511f3
vikram singh <vikram.singh@bold.com>
2022-03-01 14:56:20 +0530

    used corejs & regenerators for polyfill. Now supports 'includes', 'object.values', 'async-await', 'promises', etc

--- src/barbarian.js
+++ src/barbarian.js
@@ -3,3 +3,9 @@ export const wizard = 'wizard-ali'
 export function getBarbarian() {
   return 'barbarian-ali from a function';
 }
+
+export function getData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => { resolve('hi after two secs') }, 2000);
+  })
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-import { wizard, getBarbarian } from './barbarian';
+
+import { wizard, getBarbarian, getData } from './barbarian';
 import './css/style.scss';
 
+// import from other file example
 console.log('wizard: ', wizard);
 console.log('barbarian: ', getBarbarian());
 
-console.log([1, 2, 3].includes(2));
+// object/array spread example
 const obj = { a: 'alpha', b: 'bravo' };
 const newObj = { ...obj, c: 'cutea' };
 console.log(newObj);
-let y;
-const x = newObj[y ? 'b' : 'a'];
-console.log(x);
+
 const arr = [1, 23, 3];
 const arr1 = [...arr, 5];
 console.log(arr1);
+
+// includes example
+console.log([1, 2, 3].includes(2));
+
+// async-await and promise example
+getAPIData();
+
+// object.values example
+const objj = { a: 1, b: 3 };
+console.log(Object.values(objj));
+
+async function getAPIData() {
+  console.log('before promise:');
+  const result = await getData();
+  console.log(`after promise: ${result}`);
+}
